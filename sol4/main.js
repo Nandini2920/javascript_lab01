afb9eae20a614dfca5a3e1151754eca2e945831d
Nandini2920 <nandinimalviya20@gmail.com>
2024-03-26 23:20:59 +0530

    sol4

---
+++ sol4/main.js
@@ -0,0 +1,11 @@
+// main.js
+const math = require("./sol4.math.js");
+
+// Example usage
+const a = 10;
+const b = 5;
+
+console.log("Addition:", math.add(a, b));
+console.log("Subtraction:", math.subtract(a, b));
+console.log("Multiplication:", math.multiply(a, b));
+console.log("Division:", math.divide(a, b));
